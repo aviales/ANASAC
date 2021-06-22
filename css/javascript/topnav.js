6fe6a10ad357a1c7f7a3d0aed27f466ba43398ec
Alonso Viales <avialesarrieta@gmail.com>
2021-06-21 18:42:41 -0600

    scroll to top

--- css/javascript/topnav.js
+++ css/javascript/topnav.js
@@ -38,4 +38,26 @@ function myFunction() {
         });
     });
 
+    /* ..............................................
+        Scroll To Top
+        ................................................. */
+
+    $(document).ready(function() {
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#scroll-to-top').fadeIn();
+            } else {
+                $('#scroll-to-top').fadeOut();
+            }
+        });
+
+        $('#scroll-to-top').click(function() {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+
+    });
 }(jQuery));
